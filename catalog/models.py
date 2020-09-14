cb924863d5398cd708dec96bb53d47df4e8eaf2b
Serg Kopylov <lypokgres@gmail.com>
2020-09-14 18:53:58 +0700

    Добавил изображения

--- catalog/models.py
+++ catalog/models.py
@@ -5,29 +5,42 @@ class Category(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='child')
     name = models.CharField(max_length=20)
     slug = models.SlugField(blank=True, null=True, unique=True)
-
-    def get_url(self):
-        url = []
-        if self.parent:
-            url += [self.parent.get_url()]
-            url += [self.slug]
-        else:
-            return self.slug
-        return '/'.join(url)
+    image = models.ImageField(upload_to='image', blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def get_url(self):
+        if self.parent:
+            url = self.parent.get_url()
+        else:
+            return '{}/'.format(self.slug)
+        return '{}{}/'.format(url, self.slug)
+
+    def get_img(self):
+        if self.image:
+            return self.image.url
+        elif self.parent.image:
+            return self.parent.image.url
+        else:
+            return "http://placehold.it/100x100"
 
 
 class Good(models.Model):
-    image = models.ImageField(blank=True)
+    image = models.ImageField(upload_to='image', blank=True, null=True)
     name = models.CharField(max_length=20)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='good')
     slug = models.SlugField(blank=True, null=True, unique=True)
-
-    def get_url(self):
-        url = self.category.get_url() + '/' + str(self.id)
-        return url
 
     def __str__(self):
         return self.name
+
+    def get_url(self):
+        url = '{}{}_{}/'.format(self.category.get_url(), self.slug, str(self.id))
+        return url
+
+    def get_img(self):
+        if self.image:
+            return self.image.url
+        else:
+            return self.category.get_img()
--- catalog/views.py
+++ catalog/views.py
@@ -8,17 +8,27 @@ def index(request):
     goods_list = Good.objects.all()
     paginator = Paginator(goods_list, 12)
     page = request.GET.get('page')
+    # current_slug = page.strip('/').split('/')[-1]
+    # filtered_goods = Good.objects.get()
     try:
         goods = paginator.page(page)
     except PageNotAnInteger:
         goods = paginator.page(1)
     except EmptyPage:
         goods = paginator.page(paginator.num_pages)
-    return render(request, 'catalog/index.html', {'categories_list': categories_list,
-                                                  'goods': goods})
+    context = {
+        'goods': goods,
+        'categories_list': categories_list
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def good_page(request, url):
     item = url.strip('/').split('/')[-1]
     good = Good.objects.get(id=item)
-    return render(request, 'catalog/good_page.html', {'good': good})
+    categories_list = Category.objects.all()
+    context = {
+        'good': good,
+        'categories_list': categories_list
+    }
+    return render(request, 'catalog/good_page.html', context)
