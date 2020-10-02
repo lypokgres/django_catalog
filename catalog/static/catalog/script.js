01731ebcc1bf78187ade632b4a9bed43c103b635
Serg Kopylov <lypokgres@gmail.com>
2020-10-02 15:06:44 +0700

    Добавил обработку ошибок в ajax поиске

--- catalog/static/catalog/script.js
+++ catalog/static/catalog/script.js
@@ -1,26 +1,35 @@
-$("#search__form").on("submit", function () {
-    $.ajax({
-        url: '/search_ajax/',
-        type: 'post',
-        data: {
-            query: $("#search__input").val(),
-            csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-        },
-        dataType: 'json',
-        success: function (data) {
-            $('.good').html(data['search_ajax'])
+
+    $("#search__form").on("submit", function () {
+        $.ajax({
+            url: '/search_ajax/',
+            type: 'post',
+            data: {
+                query: $("#search__input").val(),
+                csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+            },
+            dataType: 'json',
+            success: [
+                function (data) {
+                    $('.good').html(data['search_ajax'])
+                }
+            ],
+            error: [
+                function () {
+                    alert('Oops!')
+                }
+            ]
+        })
+        return false
+    });
+
+    var $buttons = $(".category__button");
+
+    $buttons.on('click', function () {
+        var $this = $(this)
+        if ($this.next().hasClass('first')) {
+            $('.category__list').not($this.next()).removeClass('show')
+            $buttons.not($this).removeClass('active')
         }
+        $this.toggleClass('active')
+        $this.next().toggleClass('show')
     })
-    return false
-});
-
-const buttons = $(".category__button");
-
-buttons.on('click', function () {
-    if ($(this).attr("id") === 'None') {
-        $('.category__list').not($(this).next()).removeClass('show')
-        $('.category__button').not($(this)).removeClass('active')
-    }
-    $(this).toggleClass('active')
-    $(this).next().toggleClass('show')
-})
